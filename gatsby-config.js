ad1d5befba37f47ec43a6735db9e1d2bbc825378
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2023-04-28 10:00:34 -0600

    add: replace site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Ezra John Guia',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Ezra John Guia is a DevOps specialist who excels in delivering software through the use of efficient and collaborative software development practices.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    // twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Ezra John Guia',
+        short_name: 'Ezra John Guia',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
