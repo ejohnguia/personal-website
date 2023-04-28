efee68dd97c4b0fc7c9f2bfd9505d7562fe63263
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2023-04-28 09:45:21 -0600

    add: replace name and company in hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,26 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Ezra John Guia.</h2>;
+  const three = (
+    <h3 className="big-heading">
+      I provide fast software delivery through teamwork between developers and operations
+    </h3>
+  );
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software developer DevOps specialist focused on fast teamwork to deliver exceptional
+        digital experiences{' '}
+        <a href="https://www.bell.ca/" target="_blank" rel="noreferrer">
+          Bell
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
